9f231d64b733068001de86694387c9a56ea88e50
Mohammed Jalal Jaleh <jalaljaleh@gmail.com>
2022-09-30 13:56:52 +0330

    Add files via upload

---
+++ Templetes/Window.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Wpf.Styles.Templetes
+{
+    public partial class WindowStyle : ResourceDictionary
+    {
+        public WindowStyle()
+        {
+            InitializeComponent();
+            App.OnLockChanged += App_OnLockChanged;
+        }
+
+        private void App_OnLockChanged(bool isLocked)
+        {
+
+        }
+
+        private void BtnExit_Click(object sender, RoutedEventArgs e)
+        {
+            var window = (Window)((FrameworkElement)sender).TemplatedParent;
+            window.Close();
+
+        }
+
+        private void BtnMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            var window = (Window)((FrameworkElement)sender).TemplatedParent;
+            if (window.WindowState == System.Windows.WindowState.Normal)
+            {
+                window.WindowState = System.Windows.WindowState.Maximized;
+            }
+            else
+            {
+                window.WindowState = System.Windows.WindowState.Normal;
+            }
+        }
+
+        private void BtnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            var window = (Window)((FrameworkElement)sender).TemplatedParent;
+            window.WindowState = System.Windows.WindowState.Minimized;
+        }
+
+        private void BtnLock_Click(object sender, RoutedEventArgs e)
+        {
+            App.IsLocked = true;
+        }
+    }
+}
